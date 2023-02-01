531a0144c3cd4caf1e6d288f098feb17a4d030a3
Adam Pečev <pecead@centrum.cz>
2023-02-01 15:32:58 +0100

    type erasure

--- Messaging/Sources/Messaging/JSON/JSONDecodableDispatcher.swift
+++ Messaging/Sources/Messaging/JSON/JSONDecodableDispatcher.swift
@@ -10,62 +10,77 @@ import Foundation
 public final class JSONDecodableDispatcher: DecodableDispatcher, DecodableDispatcherConnector {
 
     private let decoder: JSONDecoder
+    private var handlers: [AnyHandler] = []
 
-    private var messageMetaTypes: [Decodable.Type] = []
-    private var handlerDict: [Int: [any Handler]] = [:]
-    
     public init(decoder: JSONDecoder) {
         self.decoder = decoder
     }
-    
+
     public func register<X>(handler: X) where X: Handler, X.Message : Decodable {
-        let messageMetaTypeIndex: Int
-        let index = messageMetaTypes.firstIndex { $0 is X.Message.Type }
-
-        if let index = index {
-            messageMetaTypeIndex = index
-        } else {
-            messageMetaTypeIndex = messageMetaTypes.count
-            messageMetaTypes.append(X.Message.self)
-        }
-
-        if let handlerArray = handlerDict[messageMetaTypeIndex] {
-            var array = handlerArray
-            array.append(handler)
-            handlerDict.updateValue(array, forKey: messageMetaTypeIndex)
-        } else {
-            handlerDict.updateValue([handler], forKey: messageMetaTypeIndex)
-        }
+        handlers.append(AnyHandler(handler, decoder: decoder))
     }
-    
+
     public func handle(incomingMessage: Data) -> DecodableDispatcherStatus {
 
-        for (index, messageType) in messageMetaTypes.enumerated() {
-            if let decodedMessage = try? decoder.decode(messageType, from: incomingMessage) {
-                if let handlers = handlerDict[index] {
+        var decodedMessage: Decodable?
+        var decodedMessageErrors: [(any Handler, Error)] = []
 
-                    var errors: [(any Handler, Error)] = []
+        for wrappedHandler in handlers {
+            let result = wrappedHandler.handle(incomingMessage)
 
-                    for handler in handlers {
-                        do {
-                            try myHandle(handler, message: decodedMessage)
-                            return .handled(message: decodedMessage, by: handler)
-                        } catch {
-                            errors.append((handler, error))
-                        }
-                    }
-
-                    return .handlerNotFound(message: decodedMessage, errors: errors)
-                }
+            switch result {
+            case .success(let handlerSuccess):
+                return .handled(message: handlerSuccess.message, by: handlerSuccess.handler)
+            case .failure(let handlerFailure):
+                decodedMessageErrors.append((handlerFailure.handler, handlerFailure.error))
+                decodedMessage = handlerFailure.message
+            case .couldNotDecode:
+                break
             }
         }
 
-        return .messageNotSupported
+        if let decodedMessage {
+            return .handlerNotFound(message: decodedMessage, errors: decodedMessageErrors)
+        } else {
+            return .messageNotSupported
+        }
     }
+}
 
-    private func myHandle<T: Handler>(_ handler: T, message: Decodable) throws {
-        // force unwrapping is not nice :)
-        try handler.handle(message: message as! T.Message)
+final class AnyHandler {
+    let handle: (Data) -> HandlerResult
+    private let decoder: JSONDecoder
+
+    init<T: Handler>(_ handler: T, decoder: JSONDecoder) where T.Message: Decodable {
+        self.decoder = decoder
+
+        self.handle = { data in
+            if let message = try? decoder.decode(T.Message.self, from: data) {
+                do {
+                    try handler.handle(message: message)
+                    return .success(HandlerSuccess(message: message, handler: handler))
+                } catch {
+                    return .failure(HandlerFailure(message: message, handler: handler, error: error))
+                }
+            }
+            return .couldNotDecode
+        }
     }
-    
+}
+
+enum HandlerResult {
+    case success(HandlerSuccess)
+    case failure(HandlerFailure)
+    case couldNotDecode
+}
+
+struct HandlerSuccess {
+    let message: Decodable
+    let handler: any Handler
+}
+
+struct HandlerFailure: Error {
+    let message: Decodable
+    let handler: any Handler
+    let error: Error
 }
